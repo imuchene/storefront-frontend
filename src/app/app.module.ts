32485fe2e1496a86b988e510e19c5b360a7aa4c0
Isaiah Muchene <imuchene@msn.com>
2022-11-08 17:15:45 +0300

    First iteration of creating our store's actions and reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { StoreModule } from '@ngrx/store';
+import { shopReducer } from './store/reducer';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,6 +16,8 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot({ shop: shopReducer }),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
