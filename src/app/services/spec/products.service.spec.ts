2bc223a9195ee9e29d6655bc4de418832237cdc1
Isaiah Muchene <imuchene@msn.com>
2024-04-03 16:50:06 +0300

    Created a test for the products service

--- src/app/services/spec/products.service.spec.ts
+++ src/app/services/spec/products.service.spec.ts
@@ -1,21 +1,59 @@
 import { TestBed } from '@angular/core/testing';
-
+import { createSpyFromClass, Spy} from 'jasmine-auto-spies';
 import { ProductsService } from '../products.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 describe('ProductsService', () => {
-  let service: jasmine.SpyObj<ProductsService>;
+  
+  let service: ProductsService;
+  let httpSpy: Spy<HttpClient>;
+  const mockProductObject = {
+    name: 'Test Product 03',
+    unitPrice: 10.13,
+    description: 'Test Product 03',
+    imageUrl: 'http://www.fake-url.com/image.jpg',
+    id: '10335531-df60-4c24-9597-8ce13d841929',
+    createdAt: '2022-06-08T13:57:28.247Z',
+    updatedAt: '2022-06-08T13:57:28.247Z',
+    deletedAt: null,
+  };
+
+  const mockProducts = Array(5).fill(mockProductObject);
 
   beforeEach(() => {
-    const spy = jasmine.createSpyObj('ProductsService', ['getProducts']);
 
     TestBed.configureTestingModule({
-      providers: [{ provide: ProductsService, useValue: spy }],
+      providers: [
+        ProductsService,
+        {
+          provide: HttpClient,
+          useValue: createSpyFromClass(HttpClient),
+        }
+      ],
+     imports: [HttpClientTestingModule]
     });
 
-    service = TestBed.inject(ProductsService) as jasmine.SpyObj<ProductsService>;
+    service = TestBed.inject(ProductsService);
+    httpSpy = TestBed.inject<any>(HttpClient);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return an expected list of products', (done: DoneFn) => {
+    httpSpy.get.and.nextWith(mockProducts);
+
+    service.getProducts().subscribe({
+      next: (products) => {
+        expect(products).toBe(mockProducts);
+        done();
+      },
+      error: (e) =>  done.fail,
+    });
+
+    expect(httpSpy.get.calls.count()).toBe(1);
+  })
 });
